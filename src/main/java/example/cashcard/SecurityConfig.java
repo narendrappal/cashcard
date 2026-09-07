62fb385c7c7c54b8a006368e2ea875f1a37d7b31
narendra <learntogrowpal@gmail.com>
2026-09-07 19:08:36 +0530

    Authorization: Enable role based security, test pass now

--- src/main/java/example/cashcard/SecurityConfig.java
+++ src/main/java/example/cashcard/SecurityConfig.java
@@ -19,7 +19,7 @@ class SecurityConfig {
         http
                 .authorizeHttpRequests(request -> request
                         .requestMatchers("/cashcards/**")
-                        .authenticated())
+                        .hasRole("CARD-OWNER"))
                 .httpBasic(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable());
         return http.build();
