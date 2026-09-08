710569b779ed7bd9d657c4b937b52d703b9ce608
narendra <learntogrowpal@gmail.com>
2026-09-08 10:09:47 +0530

    Handle delete the record

--- src/test/java/example/cashcard/CashcardApplicationTests.java
+++ src/test/java/example/cashcard/CashcardApplicationTests.java
@@ -202,6 +202,28 @@ class CashcardApplicationTests {
 				.exchange("/cashcards/102", HttpMethod.PUT, request, Void.class);
 		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
 	}
+
+	@Test
+	@DirtiesContext
+	void shouldDeleteAnExistingCashCard() {
+		ResponseEntity<Void> responseEntity = restTemplate
+				.withBasicAuth("sarah1","abc123")
+				.exchange("/cashcards/99",HttpMethod.DELETE,null, Void.class);
+		assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
+
+		ResponseEntity<String> getResponse = restTemplate
+				.withBasicAuth("sarah1", "abc123")
+				.getForEntity("/cashcards/99", String.class);
+		assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+	}
+
+	@Test
+	void shouldNotDeleteACashCardThatDoesNotExist() {
+		ResponseEntity<Void> deleteResponse = restTemplate
+				.withBasicAuth("sarah1", "abc123")
+				.exchange("/cashcards/99999", HttpMethod.DELETE, null, Void.class);
+		assertThat(deleteResponse.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+	}
 	//	@Test
 //	void contextLoads() {
 //	}
